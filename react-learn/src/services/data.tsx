19997b9c801ac25bd8c223d5a31db18bef5897e4
Vasilijs Himcenko <vasilijs.himcenko@emergn.com>
2025-01-21 11:46:24 +0200

    Components concept.

---
+++ react-learn/src/services/data.tsx
@@ -0,0 +1,27 @@
+import componentsImg from '../assets/component-components.png';
+import jsxImg from '../assets/component-jsx.webp';
+import propsImg from '../assets/component-props.png';
+import stateImg from '../assets/component-states.jpg';
+
+export const CORE_CONCEPTS = [
+    {
+        image: componentsImg,
+        title: 'Components',
+        description: 'Components are the basic building blocks of React applications.'
+    },
+    {
+        image: jsxImg,
+        title: 'JSX',
+        description: 'JSX is a syntax extension to JavaScript.'
+    },
+    {
+        image: propsImg,
+        title: 'Props',
+        description: 'Props are the way to pass data from parent to child components.'
+    },
+    {
+        image: stateImg,
+        title: 'State',
+        description: 'State is a way to manage component-specific data.'
+    }
+];
