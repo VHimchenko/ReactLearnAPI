0886b90448d5d83d90d745b3f510acb7b185ed73
Vasilijs Himcenko <vasilijs.himcenko@emergn.com>
2025-01-29 17:08:02 +0200

    Ref experiments

--- react-learn/src/components/search/SetNameRoot.tsx
+++ react-learn/src/components/search/SetNameRoot.tsx
@@ -2,6 +2,7 @@ import './search.css';
 import SetName from "./SetName.tsx";
 import SetNameContent from "./SetNameContent.tsx";
 import {useState} from "react";
+import TimerChallenge from "./TimerChallenge.tsx";
 
 export default function SetNameRoot() {
     const [enteredPlayerName, setEnteredPlayerName] = useState('');
@@ -14,6 +15,12 @@ export default function SetNameRoot() {
         <>
             <SetName handleNameSubmit={handleNameSubmit}/>
             <SetNameContent nameValue={enteredPlayerName}/>
+            <div id="challenges">
+                <TimerChallenge title="Easy" targetTime={1}/>
+                <TimerChallenge title="Medium" targetTime={5}/>
+                <TimerChallenge title="Hard" targetTime={10}/>
+                <TimerChallenge title="Very hard" targetTime={15}/>
+            </div>
         </>
     )
 }
