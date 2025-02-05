a31e4e94fb574d748a0c954060ed979a0b20b89e
Vasilijs Himcenko <vasilijs.himcenko@emergn.com>
2025-02-05 11:24:58 +0200

    Added intermediate progress.

--- react-learn/src/components/shop/ShopRoot.tsx
+++ react-learn/src/components/shop/ShopRoot.tsx
@@ -1,59 +1,34 @@
 import './shop.css'
 import ShopCard from "./components/ShopCard.tsx";
-import {ICartItem, IShopCard} from "./interfaces/interfaces.ts";
+import {IShopCard} from "./interfaces/interfaces.ts";
 import Header from "./components/Header.tsx";
-import {useState} from "react";
-import {CartContext} from "./store/shopping-cart-context.tsx";
+import CartContextProvider from "./store/shopping-cart-context.tsx";
+import {DUMMY_PRODUCTS} from "../../services/data.tsx";
 
 export default function ShopRoot(){
-    const [cartItems, setCartItems] = useState<ICartItem[]>([]);
+    const productItems: IShopCard[] = DUMMY_PRODUCTS;
 
-    const productItems: IShopCard[] = [
-        {id: 1, title:"Test 1", image: "", info: "test info", price: 453.1, description: "test description"},
-        {id: 2, title:"Test 2", image: "", info: "test info", price: 453.1, description: "test description"},
-        {id: 3, title:"Test 3", image: "", info: "test info", price: 453.1, description: "test description"},
-        {id: 4, title:"Test 4", image: "", info: "test info", price: 453.1, description: "test description"},
-        {id: 5, title:"Test 5", image: "", info: "test info", price: 453.1, description: "test description"},
-        {id: 6, title:"Test 6", image: "", info: "test info", price: 453.1, description: "test description"},
-        {id: 7, title:"Test 7", image: "", info: "test info", price: 453.1, description: "test description"}
-    ];
-
-    function onAddItemToCart(item: ICartItem) {
-        const cartItem = cartItems.find(o => o.item.title === item.item.title);
-        if (cartItem) {
-            cartItem.count++
-            setCartItems([...cartItems]);
-        } else {
-            setCartItems([...cartItems, {item: item.item, count: 1}]);
-        }
-    }
-
-    function onUpdateCartQuantity(item: ICartItem, newQuantity: number) {
-        const  a = item;
-        const aa = new newQuantity;
-        return (<></>);
-    }
-
-    const shoppingCart = {
-        items: []
-    }
-
-    const ctxValue = {
-        items: shoppingCart.items,
-        addItemToCart: onAddItemToCart,
-        updateCartQuantity: onUpdateCartQuantity
-    }
-
-    return(
-        <CartContext.Provider value={ctxValue}>
-        <div id="shop">
+    return (
+        <CartContextProvider>
             <Header />
             <div className="cards-container">
                 {productItems.map(item => {
                     return <ShopCard key={item.title} cardItem={item} />;
                 })}
             </div>
-        </div>
-        </CartContext.Provider>
-    )
+        </CartContextProvider>
+    );
+
+    // return(
+    //     <CartContext.Provider value={ctxValue}>
+    //     <div id="shop">
+    //         <Header />
+    //         <div className="cards-container">
+    //             {productItems.map(item => {
+    //                 return <ShopCard key={item.title} cardItem={item} />;
+    //             })}
+    //         </div>
+    //     </div>
+    //     </CartContext.Provider>
+    // )
 }
--- react-learn/src/components/shop/store/shopping-cart-context.tsx
+++ react-learn/src/components/shop/store/shopping-cart-context.tsx
@@ -1,5 +1,6 @@
-import {createContext} from "react";
+import {createContext, useReducer, useState} from "react";
 import {ICartItem} from "../interfaces/interfaces.ts";
+import {DUMMY_PRODUCTS} from "../../../services/data.tsx";
 
 export const CartContext = createContext<{
     items: ICartItem[];
@@ -10,3 +11,136 @@ export const CartContext = createContext<{
     addItemToCart: () => {},
     updateCartQuantity: () => {}
 });
+
+export interface IReducerAction<T> {
+    type: string;
+    payload: T;
+}
+
+export interface IPayloadUpdateCartQuantity {
+    item: ICartItem;
+    amount: number;
+}
+
+export interface IShoppingCartState {
+    items: ICartItem[];
+}
+
+function shoppingCartReducer<T>(state: IShoppingCartState, action: IReducerAction<T>) {
+    switch (action.type) {
+        case 'ADD_ITEM': {
+            const updatedItems = [...state.items];
+            const existingCartItemIndex = updatedItems
+                .findIndex((cartItem) => cartItem.item.id === action.payload);
+
+            const existingCartItem = updatedItems[existingCartItemIndex];
+            if (existingCartItem) {
+                updatedItems[existingCartItemIndex] = {
+                    ...existingCartItem,
+                    count: existingCartItem.count + 1
+                };
+            } else {
+                const product = DUMMY_PRODUCTS
+                    .find(p => p.id === action.payload)!;
+                updatedItems.push({
+                    item: product,
+                    count: 1
+                });
+            }
+
+            return ({
+                ...state,
+                items: updatedItems
+            })
+        }
+        case 'UPDATE_ITEM_QUANTITY': {
+            const updatedItems = [...state.items];
+            const updatedItemIndex = updatedItems
+                .findIndex(i => i.item.id === (action.payload as IPayloadUpdateCartQuantity).item.item.id);
+
+            const updatedItem = {
+                ...updatedItems[updatedItemIndex],
+            };
+
+            updatedItem.count += (action.payload as IPayloadUpdateCartQuantity).amount;
+
+            if (updatedItem.count < 1) {
+                updatedItems.splice(updatedItemIndex, 1);
+            } else {
+                updatedItems[updatedItemIndex] = updatedItem;
+            }
+
+            return {
+                ...state,
+                items: updatedItems
+            }
+        }
+        default:
+            return state;
+    }
+}
+
+export default function CartContextProvider({children}: {children: any}) {
+    const [cartItems, setCartItems] = useState<ICartItem[]>([]);
+
+    const [shoppingCartState, shoppingCartDispatch] = useReducer(
+        shoppingCartReducer,
+        {
+            items: []
+        }
+    );
+
+    function handleAddItemToCart(item: ICartItem) {
+        shoppingCartDispatch({
+            type: 'ADD_ITEM',
+            payload: item
+        });
+
+        const cartItem = cartItems.find(o => o.item.title === item.item.title);
+        if (cartItem) {
+            cartItem.count++
+            setCartItems([...cartItems]);
+        } else {
+            setCartItems([...cartItems, {item: item.item, count: 1}]);
+        }
+    }
+
+    function onUpdateCartQuantity(item: ICartItem, amount: number) {
+        shoppingCartDispatch({
+            type: 'UPDATE_ITEM_QUANTITY',
+            payload: {
+                item: item,
+                amount: amount
+            }
+        });
+        // setShoppingCart((prevShoppingCart)=>{
+        //     const updatedItems = [...prevShoppingCart.items];
+        //     const updatedItemIndex = updatedItem.findIndex(i => i.item.id === item.item.id);
+        //
+        //     const updatedItem = {
+        //         ...updatedItems[updatedItemIndex],
+        //     };
+        //
+        //     updatedItem.count += amount;
+        //
+        //     if(updatedItem.count < 1) {
+        //         updatedItems.splice(updatedItemIndex, 1);
+        //     } else {
+        //         updatedItems[updatedItemIndex] = updatedItem;
+        //     }
+        //
+        //     return {
+        //         ...prevShoppingCart,
+        //         items: updatedItems
+        //     }
+        // });
+    }
+
+    const ctxValue = {
+        items: shoppingCartState.items,
+        addItemToCart: handleAddItemToCart,
+        updateCartQuantity: onUpdateCartQuantity
+    }
+
+    return <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+}
